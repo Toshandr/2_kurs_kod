d2bedb6aa212a15dd09bd6dbca03cb2a4d380561
Антон <antonnechlopochin@mail.ru>
2025-09-24 22:52:21 +0300

    save

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c12832b8a1e5d6d11153509051a52ab1430b014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b03a2d6f864919d1d5b172f286777324e80f196")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
